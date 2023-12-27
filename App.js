4da164907667d91908541f63f23e2c9488317789
Aijaz ahmad Dar <110050419+daraijaz1122@users.noreply.github.com>
2023-12-27 18:12:29 +0000

    added part 1

---
+++ App.js
@@ -0,0 +1,8 @@
+ 
+ const heading = React.createElement("h1",{
+    id:"heading",xyz:"abc"},
+    "hello from react");
+    console.log(heading)
+  const  Root = ReactDOM.createRoot(document.getElementById("root"))
+ Root.render(heading);
+
